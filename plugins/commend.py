5d446c4f0c4483e05ee45bc7385b2859b46af9c3
꧁༒♛Bihari ♛༒꧂ <abhi736902@gmail.com>
2025-10-15 16:54:27 +0530

    Update commend.py
    Signed-off-by: ꧁༒♛Bihari ♛༒꧂ <abhi736902@gmail.com>

--- plugins/commend.py
+++ plugins/commend.py
@@ -173,7 +173,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
 	    InlineKeyboardButton('💻 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='@BIHARICHATING_BOT')
 	],[
             InlineKeyboardButton('• ʜᴏᴍᴇ •', callback_data='start'),
-	    InlineKeyboardButton('• ᴄʟᴏsᴇ •', callback_data='closedata')
+	    InlineKeyboardButton('• ᴄʟᴏsᴇ •', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         me2 = (await client.get_me()).mention
@@ -425,7 +425,7 @@ async def delete_files_list(client, message):
 @Client.on_message(filters.command("about"))
 async def about(client, message):
     buttons = [[
-       InlineKeyboardButton('💻 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='https://github.com/Botsthe/AV-FILE-TO-LINK-PRO.git')
+       InlineKeyboardButton('💻 sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='@BIHARICHATING_BOT')
     ],[
        InlineKeyboardButton('• ᴄʟᴏsᴇ •', callback_data='close_data')
     ]]
