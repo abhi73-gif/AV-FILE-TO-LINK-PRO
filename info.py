0db3b73b14325a9a8c3ea0d50e815a47d95e1e90
꧁༒♛Bihari ♛༒꧂ <abhi736902@gmail.com>
2025-10-15 12:12:03 +0530

    Update info.py
    Signed-off-by: ꧁༒♛Bihari ♛༒꧂ <abhi736902@gmail.com>

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = list(map(int, environ.get('ADMINS', '7881009185').split()))  # List of 
 AUTH_CHANNEL = list(map(int, environ.get("AUTH_CHANNEL", "-1002102037760 -1002012150170").split()))  # Allowed channels for authorization
 
 # username add without @
-OWNER_USERNAME = environ.get("OWNER_USERNAME", '@Huffgfyujicfy_bot')  # Owner's username
+OWNER_USERNAME = environ.get("OWNER_USERNAME", '')  # Owner's username
 BOT_USERNAME = environ.get("BOT_USERNAME", 'Huffgfyujicfy_bot')  # Bot's username
 
 # 🔗 Channel & Support Links
